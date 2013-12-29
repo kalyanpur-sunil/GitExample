da7e27e7763ef254f3d11fa25dff5849ec8c2ff9
kalyanpursunil85 <kalyanpursunil85>
2013-12-29 17:49:23 -0600

    Revered to previous version

--- GitExample/src/org/sunil/GitExampleTest.java
+++ GitExample/src/org/sunil/GitExampleTest.java
@@ -4,7 +4,6 @@ public class GitExampleTest {
 	public static void main(String []args){
 		System.out.println("Initial work");
 		System.out.println("Sunil is making some changes in his branch");
-		System.out.println("Kaka worked on some enhancements");
 	}
 
 }
